05c2e8f3c89d4c355f8009ed8eda1494235eed91
shen102 <sheenrodrigazo@gmail.com>
2023-10-19 01:31:02 +0800

    Update script.js
    created script for navigation bar and for menu bar for mobile that can disappear when clicking a menu

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 // Selecting the mobile menu button
 const menu = document.querySelector('#mobile_menu');
 
@@ -18,46 +21,46 @@ const mobileMenu = () => {
 menu.addEventListener('click', mobileMenu);
 
 const highlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const breedMenu = document.querySelector('#about-breed')
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const breedMenu = document.querySelector('#about-breed');
     //const aboutUs = document.querySelector('#about-page')
-    const aboutPhoto = document.querySelector('#about-photo')
-    let scrollPos = window.scrollY
+    const aboutPhoto = document.querySelector('#about-photo');
+    let scrollPos = window.scrollY;
 
     if (window.innerWidth > 960 && scrollPos < 600) {
-        homeMenu.classList.add('highlight')
-        breedMenu.classList.remove('highlight')
-        return
+        homeMenu.classList.add('highlight');
+        breedMenu.classList.remove('highlight');
+        return;
     } else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutPhoto.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-
-        return
+        aboutPhoto.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        return;
     }
     else if (window.innerWidth > 960 && scrollPos < 2345) {
-        aboutPhoto.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-        //aboutUs.classList.remove('highlight')
-        return
+        aboutPhoto.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        //aboutUs.classList.remove('highlight');
+        return;
     }
     if ((elem && window.innerWidth < 960 && scrollPos < 600 || elem)) {
-        elem.classList.remove('highlight')
+        elem.classList.remove('highlight');
     }
-}
-window.addEventListener('scroll', highlightMenu)
-window.addEventListener('click', highlightMenu)
+};
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
 
 //close Mobile Menu when clicking menu  item
 const hideMenu = () => {
     const menuBar = document.querySelector('is-active')
     if (window.innerWidth > 768 && menuBar)
         menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active')
-}
-menuLinks.addEventListener('click', hideMenu)
-navlogo.addEventListener('click', hideMenu)
+    menuLinks.classList.remove('active');
+};
+menuLinks.addEventListener('click', hideMenu);
+navlogo.addEventListener('click', hideMenu);
 
-    
+
+
     
 
